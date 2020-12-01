93a996baf634fb76d9e14a30168eccfbf35540e0
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-01 11:48:22 -0300

    exercicio3 function

--- exercises/4.0/scriptFunction2.js
+++ exercises/4.0/scriptFunction2.js
@@ -12,3 +12,18 @@ function highterIndex(highterValue) {
   return numbers;
 } 
 console.log(highterIndex([2, 3, 6, 7, 10, 1]));
+
+//Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+//Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+//Valor esperado no retorno da função: 6 .
+
+function lowerIndex(lowerValue) {  
+  let numbers = 0;
+  for (let index in lowerValue) {
+    if (lowerValue[numbers] > lowerValue[index]) {
+      numbers = index      
+    }    
+  }
+  return numbers;
+} 
+console.log(lowerIndex([2, 4, 6, 7, 10, 0, -3]));
