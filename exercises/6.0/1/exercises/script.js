fe779f64fade4ee677cf9edb42d1132389bd59b2
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-04 19:28:00 -0300

    need to work on script

---
+++ exercises/6.0/1/exercises/script.js
@@ -0,0 +1,10 @@
+function createOptions() {
+let states = document.getElementById('estado')
+let stateOptions = document.createElement('option')
+let numberOfStates = 27
+  for(let index = 0; index = numberOfStates; index += 1){
+    states.appendChild(stateOptions);
+  }
+}
+
+
